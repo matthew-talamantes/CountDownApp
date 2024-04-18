5b3f02290e355be3c8e3fcb6fb0b44b28bda2a54
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2024-04-18 19:01:09 -0400

    Add jwt token lifetimes

--- backend/countdown/countdown/settings.py
+++ backend/countdown/countdown/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -170,6 +170,8 @@ REST_FRAMEWORK = {
 # Simple JWT settings
 SIMPLE_JWT = {
     "USER_ID_FIELD": "uuid",
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
 }
 
 # Dj rest auth settings
