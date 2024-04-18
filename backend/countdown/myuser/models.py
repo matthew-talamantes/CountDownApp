12c6c37dfc68c0636543c3b96d654e6e98b1e670
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2024-04-18 16:33:33 -0400

    Add MyUser model

--- backend/countdown/myuser/models.py
+++ backend/countdown/myuser/models.py
@@ -1,3 +1,11 @@
+import uuid
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class MyUser(AbstractUser):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+
+    def __str__(self):
+        return self.username
