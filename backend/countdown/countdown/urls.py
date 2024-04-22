44773c2c817a023474fb09f98d45d77c4d8a538f
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2024-04-22 10:48:41 -0400

    Add static urls

--- backend/countdown/countdown/urls.py
+++ backend/countdown/countdown/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 from myuser.views import CustomRegistration
 
@@ -26,3 +28,6 @@ urlpatterns = [
     path('api-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('api-auth/register/', CustomRegistration.as_view(), name='account_signup'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
