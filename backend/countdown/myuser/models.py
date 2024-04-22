b7efc63a1b5378807442269c2524a9fa2d72b988
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2024-04-22 08:50:11 -0400

    Add Profile and migrate

--- backend/countdown/myuser/models.py
+++ backend/countdown/myuser/models.py
@@ -3,9 +3,29 @@ import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from PIL import Image
+
 # Create your models here.
 class MyUser(AbstractUser):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
 
     def __str__(self):
         return self.username
+    
+class Profile(models.Model):
+    user = models.OneToOneField(MyUser, on_delete=models.CASCADE)
+    profileImage = models.ImageField(upload_to='profileImages/', blank=True, null=True)
+    friends = models.ManyToManyField(MyUser, related_name='friends', blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.profileImage.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.profileImage.path)
