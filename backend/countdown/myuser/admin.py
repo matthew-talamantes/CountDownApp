4825166c41b7d7bdf4b3f2c203158ddae82f86f1
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2024-04-22 08:55:39 -0400

    Register Profile with admin

--- backend/countdown/myuser/admin.py
+++ backend/countdown/myuser/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import MyUser
+from .models import MyUser, Profile
+
+class ProfileAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user', 'friends')
 
 # Register your models here.
 admin.site.register(MyUser)
+admin.site.register(Profile, ProfileAdmin)
