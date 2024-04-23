e84f9f64ee744fae71134d053dac177c6cb1a38c
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2024-04-23 17:32:19 -0400

    Update api registration name

--- backend/countdown/countdown/urls.py
+++ backend/countdown/countdown/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('api-auth/', include('dj_rest_auth.urls')),
     path('api-auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('api-auth/register/', CustomRegistration.as_view(), name='account_signup'),
+    path('api-auth/register/', CustomRegistration.as_view(), name='api_account_signup'),
     path('__reload__/', include('django_browser_reload.urls')),
     path('', HomeView.as_view(), name='home'),
 ]
