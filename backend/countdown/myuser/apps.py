f5efa22fba5b365ee48fdf34f0d655648cb1cbd9
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2024-04-27 11:46:22 -0400

    On user registration create profile object

--- backend/countdown/myuser/apps.py
+++ backend/countdown/myuser/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MyuserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myuser'
+
+    def ready(self):
+        import myuser.signals
