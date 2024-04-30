b327142a94abea72ebc9d8413d2e784dd630cf8a
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2024-04-30 09:51:36 -0400

    Register Countdown with admin

--- backend/countdown/counts/admin.py
+++ backend/countdown/counts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Countdown
+
 # Register your models here.
+class CountdownAdmin(admin.ModelAdmin):
+    raw_id_fields = ('created_by', 'updated_by', 'shared_with')
+
+admin.site.register(Countdown, CountdownAdmin)
