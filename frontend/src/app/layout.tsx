16b88d82257a0096b9cdfbe06375f3051f74bf72
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2024-05-07 16:35:20 -0400

    Wrap all child elements with main tags

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -21,7 +21,9 @@ export default function RootLayout({
       <body className={`${inter.className}`}>
         <Navbar />
         <Messages />
-        {children}
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   );
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,8 @@ import FavoriteCounts from "@/components/FavoriteCounts";
 
 export default function Home() {
   return (
-    <main>
+    <>
       <FavoriteCounts />
-    </main>
+    </>
   );
 }
