412b2fde3d010f285a97b4e766a659f3877f8090
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2024-05-07 10:26:26 -0400

    Update next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, {dev}) => { // Only needed on windows
+        if (dev) {
+            config.watchOptions = {
+                poll: 1000,
+                aggregateTimeout: 300,
+            }
+        }
+        return config;
+    },
+    output: 'standalone',
+};
 
 export default nextConfig;
