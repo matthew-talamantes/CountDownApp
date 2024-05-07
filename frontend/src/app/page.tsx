cc3d40073c8369f937c63bad4cc7896c28514379
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2024-05-07 15:54:42 -0400

    Move NavBar to layout and add Messages

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,9 @@
-import Navbar from "@/components/Navbar";
 import FavoriteCounts from "@/components/FavoriteCounts";
 
 export default function Home() {
   return (
-    <>
-      <Navbar />
-      <main>
-        <FavoriteCounts />
-      </main>
-    </>
+    <main>
+      <FavoriteCounts />
+    </main>
   );
 }
