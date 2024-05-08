744be518d371a15dc1cd7767751c3b1cf66b8061
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2024-05-07 20:37:05 -0400

    Update message style

--- frontend/src/components/Message.tsx
+++ frontend/src/components/Message.tsx
@@ -1,9 +1,30 @@
 
 export default function Message({ type, message }: { type: string, message: string }) {
+
+    const getClasses = () => {
+        let classes = "alert";
+        switch (type) {
+            case "success":
+                classes += " alert-success";
+                break;
+            case "error":
+                classes += " alert-error";
+                break;
+            case "warning":
+                classes += " alert-warning";
+                break;
+            case "info":
+                classes += " alert-info";
+                break;
+            default:
+                break;
+        }
+        return classes;
+    };
+
     return (
-        <article className="bg-slate-400">
-            <p>Type: {type}</p>
-            <p>Message: {message}</p>
+        <article role="alert" className={getClasses()}>
+            <h2 className="text-xl">{message}</h2>
         </article>
     );
 }
