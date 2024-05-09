bef19c8e9e46cab1100ba065c51f6c3d9cd7c6c4
Constantus <38108928+Constantus@users.noreply.github.com>
2024-05-09 13:58:12 -0400

    Bug fix

--- backend/countdown/myuser/forms.py
+++ backend/countdown/myuser/forms.py
@@ -16,7 +16,7 @@ class CustomSignupForm(SignupForm):
         self.fields['username'].widget = forms.TextInput(attrs={'class': 'grow', 'placeholder': 'Username'})
         self.fields['email'].widget = forms.EmailInput(attrs={'class': 'grow', 'placeholder': 'Email'})
         self.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'grow', 'placeholder': 'Password'})
-        self.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'grow', 'placeholder': 'Confirm Password'})
+        # self.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'grow', 'placeholder': 'Confirm Password'})
     
 class ProfileUpdateForm(ModelForm):
     class Meta:
