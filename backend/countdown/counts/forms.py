e5aa991c67df790aeb58302f0c19c7d812a24afc
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2024-05-10 13:54:35 -0400

    Move profile form to proper app

--- backend/countdown/counts/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Countdown
-
-class CountdownForm(forms.ModelForm):
-    dateTime = forms.SplitDateTimeField(widget=forms.SplitDateTimeWidget(date_attrs={'type': 'date'}, time_attrs={'type': 'time'}))
-    class Meta:
-        model = Countdown
-        fields = ['title', 'dateTime', 'timeZone', 'description', 'public_link', 'shared_with']
