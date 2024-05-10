1c8da92acd6e50f0e1f3cb79deca83b9ca6e1152
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2024-05-10 16:05:30 -0400

    Update ProfileSerializer to include favoriteCountdown and followingCountdowns fields

--- backend/countdown/userprofile/serializers.py
+++ backend/countdown/userprofile/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
 
 from myuser.serializers import MyUserSerializer
+from counts.serializers import CountdownSerializer
 
 from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     user = MyUserSerializer()
     friends = MyUserSerializer(many=True)
+    favoriteCountdown = CountdownSerializer()
+    followingCountdowns = CountdownSerializer(many=True)
+
     class Meta:
         model = Profile
-        fields = ('user', 'profileImage', 'friends')
+        fields = ('user', 'profileImage', 'friends', 'favoriteCountdown', 'followingCountdowns')
