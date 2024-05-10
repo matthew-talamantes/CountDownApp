7c751b283a1c9c0af26fda8067fdb5c1a142caef
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2024-05-10 13:43:32 -0400

    Add profile model

--- backend/countdown/userprofile/models.py
+++ backend/countdown/userprofile/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
+from PIL import Image
+from django.urls import reverse
+
+from myuser.models import MyUser
+from counts.models import Countdown
+
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(MyUser, on_delete=models.CASCADE)
+    profileImage = models.ImageField(verbose_name='Profile Image', upload_to='profileImages/', default='default.jpg', blank=True, null=True)
+    friends = models.ManyToManyField(MyUser, related_name='friends', blank=True)
+    favoriteCountdown = models.ForeignKey(Countdown, on_delete=models.SET_NULL, blank=True, null=True)
+    followingCountdowns = models.ManyToManyField(Countdown, related_name='followingCountdowns', blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    
+    def get_absolute_url(self):
+        return reverse('profile')
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        
+        try:
+            img = Image.open(self.profileImage.path)
+
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.profileImage.path)
+        except ValueError:
+            self.profileImage = 'default.jpg'
+            super().save(*args, **kwargs)
