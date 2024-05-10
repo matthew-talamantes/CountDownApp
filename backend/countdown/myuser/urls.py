032da29e83b21773b7fb4260fc1e5cbfa1d09675
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2024-05-10 13:58:20 -0400

    Remove profile views

--- backend/countdown/myuser/urls.py
+++ backend/countdown/myuser/urls.py
@@ -1,8 +1,3 @@
 from django.urls import path
-from .views import UserProfileDetailView, UserProfileUpdateView, PubicProfileDetailView
 
-urlpatterns = [
-    path('profile/', UserProfileDetailView.as_view(), name='profile'),
-    path('profile/update/', UserProfileUpdateView.as_view(), name='profile-update'),
-    path('profile/<uuid:uuid>/', PubicProfileDetailView.as_view(), name='public-profile'),
-]
+urlpatterns = []
