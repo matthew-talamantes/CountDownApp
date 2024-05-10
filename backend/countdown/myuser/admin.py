10a0363320e20c4e8c92b138348879c7da1e6fd2
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2024-05-10 13:46:07 -0400

    Move profile to userprofile app

--- backend/countdown/myuser/admin.py
+++ backend/countdown/myuser/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 
-from .models import MyUser, Profile
+from .models import MyUser
 
-class ProfileAdmin(admin.ModelAdmin):
-    raw_id_fields = ('user', 'friends')
 
 # Register your models here.
 admin.site.register(MyUser)
-admin.site.register(Profile, ProfileAdmin)
--- backend/countdown/userprofile/admin.py
+++ backend/countdown/userprofile/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Profile
+
 # Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user', 'friends')
+
+admin.site.register(Profile, ProfileAdmin)
