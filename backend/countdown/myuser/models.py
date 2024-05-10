6cadccf319b96872a59107ecbc87b8c9aff9d599
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2024-05-10 13:43:21 -0400

    Remove profile model

--- backend/countdown/myuser/models.py
+++ backend/countdown/myuser/models.py
@@ -3,9 +3,6 @@ import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from PIL import Image
-from django.urls import reverse
-
 # Create your models here.
 class MyUser(AbstractUser):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
@@ -13,27 +10,3 @@ class MyUser(AbstractUser):
     def __str__(self):
         return self.username
     
-class Profile(models.Model):
-    user = models.OneToOneField(MyUser, on_delete=models.CASCADE)
-    profileImage = models.ImageField(verbose_name='Profile Image', upload_to='profileImages/', default='default.jpg', blank=True, null=True)
-    friends = models.ManyToManyField(MyUser, related_name='friends', blank=True)
-
-    def __str__(self):
-        return f'{self.user.username} Profile'
-    
-    def get_absolute_url(self):
-        return reverse('profile')
-    
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        
-        try:
-            img = Image.open(self.profileImage.path)
-
-            if img.height > 300 or img.width > 300:
-                output_size = (300, 300)
-                img.thumbnail(output_size)
-                img.save(self.profileImage.path)
-        except ValueError:
-            self.profileImage = 'default.jpg'
-            super().save(*args, **kwargs)
