7962dc243b58ca22655f726d5b9828fa9d67c8d4
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2024-05-12 10:07:22 -0400

    Remove debugging console.log

--- frontend/src/app/api/getUserProfile/route.ts
+++ frontend/src/app/api/getUserProfile/route.ts
@@ -5,7 +5,6 @@ export const dynamic = 'force-dynamic';
 export async function GET(request: Request) {
     const session = await getSession();
     session.save();
-    console.log('Route Session: ', session);
     const profile = await getUserProfile(session);
     return Response.json(profile);
 }
